7bd9f8e3c1a079d8523650d43e7f1fb3ba98c2ad
JOONAXII-PC\Joonaxii <Joonaxii@gmail.com>
2024-02-11 23:02:11 +0200

    Add CMakeLists.txt Fix include paths in cpp files

--- imconfig.h
+++ imconfig.h
@@ -105,10 +105,10 @@
 //---- Define constructor to convert your string type to ImStrv (which is a non-owning begin/end pair)
 // This will be inlined as part of ImStrv class declaration.
 // This has two benefits: you won't need to use .c_str(), if length is already computed it is faster.
-//#include <string>
-//#include <string_view>
+#include <string>
+#include <string_view>
 //#define IM_STRV_CLASS_EXTRA    ImStrv(const std::string& s)       { Begin = s.c_str(); End = Begin + s.length(); }
-//#define IM_STRV_CLASS_EXTRA    ImStrv(const std::string_view& s)  { Begin = s.data(); End = Begin + s.length(); }
+#define IM_STRV_CLASS_EXTRA    ImStrv(const std::string_view& s)  { Begin = s.data(); End = Begin + s.length(); }
 //#define IM_STRV_CLASS_EXTRA    ImStrv(const MyString& s)          { Begin = s.Data; End = s.end(); }
 
 //---- Use 32-bit vertex indices (default is 16-bit) is one way to allow large meshes with more than 64K vertices.
